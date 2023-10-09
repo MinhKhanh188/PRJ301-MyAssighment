55d174ab4f43c0ea1694e8b6aac75ea35cc083fd
MinhKhanh188 <jokhanh188@gmail.com>
2023-10-09 21:54:54 +0700

    Add a databe

---
+++ MyProjectPRJ301/src/java/dal/DBConnect.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sonnt
+ */
+public class DBConnect {
+    public Connection connection;
+
+    public DBConnect(String url, String username, String pass) {
+        try {
+            //goi driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //conect
+            connection = DriverManager.getConnection(url, username, pass);
+            System.out.println("Connected");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public DBConnect() {
+        this("jdbc:sqlserver://localhost:1433;databaseName=PRJ301_FALL2023_Afternoon",
+                "khanh188", "123456");
+    }
+    public ResultSet getData(String sql){
+        ResultSet rs=null;
+         Statement state;
+        try {
+            state = connection.createStatement(
+                    ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+             rs = state.executeQuery(sql);
+        } catch (SQLException ex) {
+           ex.printStackTrace();
+        }
+        return rs;
+    }
+
+    public static void main(String[] args) {
+        new DBConnect();
+    }
+}
+
+
+
+
+
+
+
