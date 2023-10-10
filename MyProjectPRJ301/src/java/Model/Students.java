8429ba52e85e72f5a5c3e2c712c660ab3edba9f8
MinhKhanh188 <jokhanh188@gmail.com>
2023-10-10 20:14:30 +0700

    Made some model

---
+++ MyProjectPRJ301/src/java/Model/Students.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Mr.Khanh
+ */
+public class Students {
+
+    private int student_id;
+    private String student_name;
+    private String student_major;
+    private String student_gender;
+    private String student_dob;
+
+    public int getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getStudent_name() {
+        return student_name;
+    }
+
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public String getStudent_major() {
+        return student_major;
+    }
+
+    public void setStudent_major(String student_major) {
+        this.student_major = student_major;
+    }
+
+    public String getStudent_gender() {
+        return student_gender;
+    }
+
+    public void setStudent_gender(String student_gender) {
+        this.student_gender = student_gender;
+    }
+
+    public String getStudent_dob() {
+        return student_dob;
+    }
+
+    public void setStudent_dob(String student_dob) {
+        this.student_dob = student_dob;
+    }
+
+    
+    
+}
